26ea42e35842178c1351c4a26abedba5f64210d3
asan6602 <110870409+asan6602@users.noreply.github.com>
2023-03-31 20:34:08 -0400

    implemented readText, changed mvn version
    Used a lambda expression, so had to change mvn version from 1.7 to 1.8.

--- solver/src/main/java/com/letterboxedsolver/readText.java
+++ solver/src/main/java/com/letterboxedsolver/readText.java
@@ -1,5 +1,8 @@
 package com.letterboxedsolver;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -12,7 +15,53 @@ public class readText {
      */
     public HashMap<Character, ArrayList<String>> read(String path) {
         HashMap<Character, ArrayList<String>> result = new HashMap<>();
+        try {
 
+            FileReader fr = new FileReader(path);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while(line != null) {
+                
+                line = line.toLowerCase();
+
+                if(line.length() < 3) {
+                    line = br.readLine();
+                    continue;
+                }
+
+                //check if line only contains letters
+                if(!line.matches("[a-zA-Z]+")) {
+                    line = br.readLine();
+                    continue;
+                }
+
+                //check if line does not have back to back letters
+                boolean doubleLetter = false;
+                for (int i = 0; i < line.length() - 1; i++) {
+                    if (line.charAt(i) == line.charAt(i + 1)) {
+                        doubleLetter = true;
+                    }
+                }
+
+                if(!doubleLetter) {
+                    char key = line.charAt(0);
+
+                    //computeIFAbsent returns the new or old value associated with the specified key
+                    result.computeIfAbsent(key, k -> new ArrayList<>()).add(line);
+                }
+
+                line = br.readLine();
+            }
+
+            br.close();
+            fr.close();
+
+        }
+        catch(IOException e) {
+            System.out.print("File Not Found");
+        }
 
         return result;
     }
