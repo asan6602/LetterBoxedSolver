4093db43b71eadae270dddb03857405406b018fc
asan6602 <110870409+asan6602@users.noreply.github.com>
2023-03-31 21:18:01 -0400

    testFindWordsInProblem

--- solver/src/test/java/com/letterboxedsolver/gameTest.java
+++ solver/src/test/java/com/letterboxedsolver/gameTest.java
@@ -1,5 +1,48 @@
 package com.letterboxedsolver;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class gameTest {
-    
+    private readText rt = new readText();
+    private String path = "data\\wordsTest2.txt";
+    private game g = new game(path);
+    private ArrayList<Character> characterList = new ArrayList<>();
+    private String letters = "abcdefghijkl";
+    ArrayList<ArrayList<Character>> rows = new ArrayList<>();
+
+    @Before
+	public void setUp() throws Exception {
+        for(int i =0; i < letters.length(); i++) {
+            this.characterList.add(letters.charAt(i));
+        }
+        
+	}
+
+    @Test
+    public void testFindWordsInProblem() {
+        HashMap<Character, ArrayList<String>> expected = new HashMap<>();
+        ArrayList<String> words = new ArrayList<>();
+        words.add("ale");
+        words.add("abel");
+        words.add("adelgidae");
+        expected.put('a', words);
+
+        HashMap<Character, ArrayList<String>> actual = g.findWordsInProblem(characterList);
+
+        assertEquals(expected, actual);
+        
+    }
+
+    @Test
+    public void testCheckRows() {
+
+    }
+
 }
